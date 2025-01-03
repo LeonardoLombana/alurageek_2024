ebe405c57062b0c50aff5f920ef88ffe2558c702
LeonardoLombana <40215598+LeonardoLombana@users.noreply.github.com>
2025-01-03 10:35:05 -0500

    Update printProducts.js

--- js/controller/printProducts.js
+++ js/controller/printProducts.js
@@ -7,11 +7,11 @@ function printProducts() {
         .then(response => response.json())
         .then(data => {
             data.forEach(element => {
-                // Crear un nuevo elemento de imagen (SVG) para cada producto
+                
                 let svg = document.createElement('img');
                 svg.src = '../img/btn-borrar.svg';
                 
-                // Crear el HTML del producto 
+                
                 let productHTML = `
                     <div class="cart" id="${element.id}">
                         <img src="${element.img}" alt="img-producto">
@@ -22,12 +22,10 @@ function printProducts() {
                     </div>
                 `;
                 
-                // Agregar el elemento SVG al contenedor de productos
                 let div = document.createElement('div');
                 div.innerHTML = productHTML;
                 div.querySelector('.price').appendChild(svg);
                 
-                // Agregar el producto al contenedor principal
                 products.appendChild(div);
 
                 svg.addEventListener('click', (event) => {

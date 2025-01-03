917822b00db48136fad7ab64c2779c0f74d3c9fd
LeonardoLombana <40215598+LeonardoLombana@users.noreply.github.com>
2025-01-03 10:34:28 -0500

    Update deletProducts.js

--- js/controller/deletProducts.js
+++ js/controller/deletProducts.js
@@ -1,12 +1,12 @@
 function deleteProduct(productId) {
-    // Enviar una solicitud DELETE al servidor para eliminar el producto
+    
     fetch(`http://localhost:3000/products/${productId}`, {
         method: 'DELETE'
     })
     .then(response => {
         if (response.ok) {
             console.log("Producto eliminado exitosamente");
-            // Eliminar el producto del DOM
+            
             let productToRemove = document.getElementById(productId);
             productToRemove.remove();
         } else {
